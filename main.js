31fbe886938e82cc1106c2d7f81d307ee6887145
PechPavlo <61708124+PechPavlo@users.noreply.github.com>
2020-05-05 19:14:50 +0300

    new

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 "use strict";
 
+let numbers = [1, 2, 3, 4, 5];
+let newNumbers = [...numbers,6];
+console.log(newNumbers);
+numbers[5] = 0;
+newNumbers = [...numbers,6];
+console.log(newNumbers);
+console.log(numbers);
+
+
 function loadDocMethod1() {
     fetch(`https://jsonplaceholder.typicode.com/comments`)
     .then(response => response.json())
@@ -13,15 +22,16 @@ async function loadDocMethod2() {
     };
 
 function getTen(param,method) {
-console.log(param.length)
+    console.log('param',param);
+    console.log(param.length);
     let commentPageCount = 0;
-     let ourTen = [];
-     let results = document.querySelector(`#results${method}`);
-     console.log(results)
-     document.querySelector(`#page-number${method}`).innerHTML = `Page ${commentPageCount/10 + 1}`;
-for (let i = commentPageCount; i < commentPageCount +10; i++) {
-    ourTen.push(`<br>${i+1}.<br> <strong>email - </strong> ${param[i].email} <br> <strong> name - </strong>
-    ${param[i].name}<br><strong> comment - </strong> "${param[i].body}"`)
+    let ourTen = [];
+    let results = document.querySelector(`#results${method}`);
+    console.log(results)
+    document.querySelector(`#page-number${method}`).innerHTML = `Page ${commentPageCount/10 + 1}`;
+    for (let i = commentPageCount; i < commentPageCount +10; i++) {
+        ourTen.push(`<br>${i+1}.<br> <strong>email - </strong> ${param[i].email} <br> <strong> name - </strong>
+        ${param[i].name}<br><strong> comment - </strong> "${param[i].body}"`)
     results.innerHTML = ourTen ;
 };
 let nextButton = document.querySelector(`#next${method}`);
